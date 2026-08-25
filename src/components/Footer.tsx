a551a40dad1195ed4592bb2f2ae02cd61a4ff2cb
anushaanupittala-lgtm <anushaanupittala@gmail.com>
2026-08-25 15:34:30 +0530

    Add Footer component with copyright and navigation

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,1 +1,17 @@
+function Footer() {
+  return (
+    <footer className="footer">
+      <div className="container footer-container">
+        <p>
+          © 2026 Alex Morgan. All rights reserved.
+        </p>
 
+        <a href="#home">
+          Back to top ↑
+        </a>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
